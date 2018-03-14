59ca3b96d367be96e198ac283d64c0106be9f8a4
서상원 <zhao@192.168.nate.com>
2018-03-14 15:58:06 +0900

    add color

--- changeColor/ViewController.swift
+++ changeColor/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var index : Int = 0
+    var index = 0
     
     @IBOutlet weak var lblColor: UILabel!
     
@@ -24,25 +24,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func btClick(_ sender: Any) {
-        if(index == 0) {
-            view.backgroundColor = UIColor.cyan
-            index = index + 1
+//        if(index == 0) {
+//            view.backgroundColor = UIColor.cyan
+//            index = index + 1
+//            lblColor.text = "cyan"
+//
+//        }
+//        else if(index == 1) {
+//            view.backgroundColor = UIColor.blue
+//            index = index + 1
+//            lblColor.text = "blue"
+//        }
+//        else if (index == 2){
+//            view.backgroundColor = UIColor.yellow
+//            index = index - 2
+//            lblColor.text = "yellow"
+//        }
+        switch(index) {
+            case 0 :
+                view.backgroundColor = UIColor.cyan;
             lblColor.text = "cyan"
-            
-        }
-        else if(index == 1) {
-            view.backgroundColor = UIColor.blue
+                index += 1
+        case  1: view.backgroundColor = UIColor.blue;
             index = index + 1
             lblColor.text = "blue"
-        }
-        else if (index == 2){
-            view.backgroundColor = UIColor.yellow
-            index = index - 2
+        default : view.backgroundColor = UIColor.yellow;
+            index = index - 2;
             lblColor.text = "yellow"
-        }
     }
     
 
-    
+    }
 }
 
